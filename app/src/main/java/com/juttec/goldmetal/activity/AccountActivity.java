29e39cdc0a6aecc017d65f7f9a6b50676ea1bf02
kkk <565944210@qq.com>
2015-09-21 14:59:03 +0800

    修改登录注册的小问题

--- app/src/main/java/com/juttec/goldmetal/activity/AccountActivity.java
+++ app/src/main/java/com/juttec/goldmetal/activity/AccountActivity.java
@@ -11,18 +11,37 @@ import android.widget.TextView;
 import com.juttec.goldmetal.R;
 import com.juttec.goldmetal.dialog.MyAlertDialog;
 
+/**
+ * 用户基本信息界面
+ */
 public class AccountActivity extends AppCompatActivity implements View.OnClickListener {
 
-    RelativeLayout head;
+    private RelativeLayout head;//头部布局
 
-    Button bt_nickname, bt_name, bt_phone, bt_qq;
-    TextView tv_nickname, tv_name, tv_phone, tv_qq;
+    private Button bt_nickname, bt_name, bt_phone, bt_qq;
+
+    private TextView tv_nickname, tv_name, tv_phone, tv_qq;
+
+    private Button btn_exit;//退出当前账号
+    private TextView tv_change_pwd;//修改密码
+
+    private MyAlertDialog dialog;//对话框
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_account);
+
+
+
+        initView();
+        dialog = new MyAlertDialog(AccountActivity.this);
+    }
+
+    private void initView() {
+
         head = (RelativeLayout) this.findViewById(R.id.head_layout);
+
         TextView lefttext = (TextView) head.findViewById(R.id.left_text);
         lefttext.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -31,15 +50,14 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
             }
         });
 
-
-        initView();
-    }
-
-    private void initView() {
         bt_nickname = (Button) this.findViewById(R.id.account_change_nickname);
         bt_name = (Button) this.findViewById(R.id.account_change_name);
         bt_phone = (Button) this.findViewById(R.id.account_change_phone);
         bt_qq = (Button) this.findViewById(R.id.account_change_qq);
+
+        tv_change_pwd = (TextView) findViewById(R.id.tv_change_pwd);
+
+        btn_exit = (Button) findViewById(R.id.btn_exit);
 
 
         tv_nickname = (TextView) this.findViewById(R.id.account_tv_nickname);
@@ -47,10 +65,14 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
         tv_phone = (TextView) this.findViewById(R.id.account_tv_phone);
         tv_qq = (TextView) this.findViewById(R.id.account_tv_qq);
 
+
+
         bt_nickname.setOnClickListener(this);
         bt_name.setOnClickListener(this);
         bt_phone.setOnClickListener(this);
         bt_qq.setOnClickListener(this);
+        tv_change_pwd.setOnClickListener(this);
+        btn_exit.setOnClickListener(this);
     }
 
     @Override
@@ -58,24 +80,41 @@ public class AccountActivity extends AppCompatActivity implements View.OnClickLi
         switch (v.getId()) {
 
             case R.id.account_change_nickname:
-
+                showDialog("昵称修改","请输入昵称",tv_nickname);
 
                 break;
+
             case R.id.account_change_name:
+                showDialog("名字修改","请输入姓名",tv_name);
+
                 break;
+
             case R.id.account_change_phone:
+
                 break;
+
             case R.id.account_change_qq:
+                showDialog("QQ号码修改","请输入QQ号码",tv_qq);
+                break;
+
+            case R.id.tv_change_pwd:
+                startActivity(new Intent(AccountActivity.this,ChangePWDActivity.class));
                 break;
         }
     }
-    public  void showDialog(String title,String edittext){
-        final MyAlertDialog dialog = new MyAlertDialog(AccountActivity.this);
+
+
+    public  void showDialog(String title,String edittext, final TextView tv){
+
         dialog.builder()
-                .setTitle("昵称修改").setEditText("请输入昵称")
+                .setTitle(title).setEditText(edittext)
                 .setSingleButton("确定", new View.OnClickListener() {
                     @Override
                     public void onClick(View v) {
+                        if (dialog.getResult()!=null) {
+                            tv.setText(dialog.getResult());
+                        }
+
                         dialog.dismiss();
                     }
                 }).show();
--- app/src/main/java/com/juttec/goldmetal/activity/LoginActivity.java
+++ app/src/main/java/com/juttec/goldmetal/activity/LoginActivity.java
@@ -2,19 +2,32 @@ package com.juttec.goldmetal.activity;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.RelativeLayout;
+import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.CompoundButton.OnCheckedChangeListener;
+import android.widget.EditText;
 import android.widget.TextView;
-
 import com.juttec.goldmetal.R;
 
 /**
  * Created by Jeremy on 2015/9/15.
+ * 登录界面
  */
-public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener,OnCheckedChangeListener{
+
+    private EditText mUserName;//用户名
+    private EditText mPwd;//密码
+
+    private CheckBox cb_remember;//记住密码
+    private TextView tv_forget;//忘记密码
+
+    private Button btn_login;//登录
+
+    private TextView tvRegister;//注册
 
 
     @Override
@@ -27,34 +40,72 @@ public class LoginActivity extends AppCompatActivity implements View.OnClickList
 
     }
 
-    private void initView() {
+
+
+
+    /**
+     * 初始化控件
+     */
+    private void initView(){
+
         RelativeLayout head = (RelativeLayout) this.findViewById(R.id.head_layout);
-
-        Button btLogin = (Button) findViewById(R.id.login_btn);
-        btLogin.setOnClickListener(this);
-
-        TextView btFergotPWD = (TextView) findViewById(R.id.login_tv_fergotpwd);
-        btFergotPWD.setOnClickListener(this);
-
-
-        TextView tvRegister = (TextView) head.findViewById(R.id.right_text);
+        tvRegister = (TextView) head.findViewById(R.id.right_text);
         tvRegister.setOnClickListener(this);
 
+        mUserName = (EditText) findViewById(R.id.et_name);
+        mUserName.setOnClickListener(this);
 
+        mPwd = (EditText) findViewById(R.id.et_pwd);//密码
+        mPwd.setOnClickListener(this);
+
+        cb_remember = (CheckBox) findViewById(R.id.cb_remember);
+        cb_remember.setOnCheckedChangeListener(this);
+
+
+        tv_forget = (TextView) findViewById(R.id.login_tv_fergotpwd);
+        tv_forget.setOnClickListener(this);
+
+        btn_login = (Button) findViewById(R.id.login_btn);
+        btn_login.setOnClickListener(this);
     }
+
+
+
+
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
+        Intent intent ;
+
+        switch (v.getId()){
             case R.id.login_btn:
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                intent = new Intent(LoginActivity.this, MainActivity.class);
+                startActivity(intent);
+                finish();
                 break;
+
+
+
             case R.id.login_tv_fergotpwd:
-                startActivity(new Intent(LoginActivity.this, FindBcakPWDActivity.class));
+                intent = new Intent(LoginActivity.this, FindBcakPWDActivity.class);
+                startActivity(intent);
                 break;
+
             case R.id.right_text:
-                startActivity(new Intent(LoginActivity.this,RegisterActivity.class));
+                intent = new Intent(LoginActivity.this,RegisterActivity.class);
+                startActivity(intent);
                 break;
         }
+
     }
+
+
+
+
+    @Override
+    public void onCheckedChanged(CompoundButton buttonView, boolean isChecked) {
+
+    }
+
+
 }
