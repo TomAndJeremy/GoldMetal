0cc5f00e4a44bf11c404f3e8406aa43e6808aaf6
jeremy <loongenix@gmail.com>
2015-09-21 14:05:40 +0800

    123

--- app/src/main/java/com/juttec/goldmetal/activity/FreeRemindActivity.java
+++ app/src/main/java/com/juttec/goldmetal/activity/FreeRemindActivity.java
@@ -2,17 +2,48 @@ package com.juttec.goldmetal.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.LinearLayout;
 
 import com.juttec.goldmetal.R;
 
 /**
  * 现货白银免费提醒
  */
-public class FreeRemindActivity extends AppCompatActivity {
+public class FreeRemindActivity extends AppCompatActivity implements View.OnClickListener {
+    private LinearLayout remindContent;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_free_remind);
+
+        initView();
+    }
+
+    private void initView() {
+        remindContent = (LinearLayout) this.findViewById(R.id.point_remind_content);
+        Button btAdd = (Button) this.findViewById(R.id.remind_bt_add);
+        btAdd.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.remind_bt_add:
+                addView();
+        }
+    }
+
+    private void addView() {
+        LinearLayout.LayoutParams lp = new LinearLayout.LayoutParams(
+                LinearLayout.LayoutParams.FILL_PARENT, LinearLayout.LayoutParams.WRAP_CONTENT);
+
+        LayoutInflater inflater = LayoutInflater.from(this);
+        View view = inflater.inflate(R.layout.point_remind_layout, null);
+        view.setLayoutParams(lp);
+        remindContent.addView(view);
     }
 }
--- app/src/main/java/com/juttec/goldmetal/activity/LoginActivity.java
+++ app/src/main/java/com/juttec/goldmetal/activity/LoginActivity.java
@@ -5,31 +5,55 @@ import android.os.Bundle;
 import android.os.PersistableBundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Button;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
 
 import com.juttec.goldmetal.R;
 
 /**
  * Created by Jeremy on 2015/9/15.
  */
-public class LoginActivity extends AppCompatActivity {
+public class LoginActivity extends AppCompatActivity implements View.OnClickListener {
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        findViewById(R.id.login_btn).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, MainActivity.class));
-            }
-        });
-        findViewById(R.id.login_tv_fergotpwd).setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View v) {
-                startActivity(new Intent(LoginActivity.this, RegisterActivity.class));
-            }
-        });
+
+        initView();
 
     }
 
+    private void initView() {
+        RelativeLayout head = (RelativeLayout) this.findViewById(R.id.head_layout);
+
+        Button btLogin = (Button) findViewById(R.id.login_btn);
+        btLogin.setOnClickListener(this);
+
+        Button btFergotPWD = (Button) findViewById(R.id.login_btn);
+        btFergotPWD.setOnClickListener(this);
+
+
+        TextView tvRegister = (TextView) head.findViewById(R.id.right_text);
+        tvRegister.setOnClickListener(this);
+
+
+    }
+
+    @Override
+    public void onClick(View v) {
+        switch (v.getId()) {
+            case R.id.login_btn:
+                startActivity(new Intent(LoginActivity.this, MainActivity.class));
+                break;
+            case R.id.login_tv_fergotpwd:
+                startActivity(new Intent(LoginActivity.this, FindBcakPWDActivity.class));
+                break;
+            case R.id.right_text:
+                break;
+        }
+    }
 }
