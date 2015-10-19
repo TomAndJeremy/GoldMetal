ceb325191c4cc6ec51baf360652d8d61c306723d
kkk <565944210@qq.com>
2015-10-19 14:44:08 +0800

    设置界面  完善 ：屏幕常亮 行情时间间隔

--- app/src/main/java/com/juttec/goldmetal/activity/AnnouncementActivity.java
+++ app/src/main/java/com/juttec/goldmetal/activity/AnnouncementActivity.java
@@ -46,6 +46,7 @@ public class AnnouncementActivity extends AppCompatActivity implements SwipeRefr
     MyAdapter myAdapter;
 
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
